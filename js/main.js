612039c8ded53c3d70cfbc79df11fd5fd39cbf0d
Taymur <htaymur12@gmail.com>
2021-09-02 17:48:00 +0600

    added all

--- js/main.js
+++ js/main.js
@@ -1,36 +1,66 @@
-const btnSearch = () =>{
+const errordiv = document.getElementById('errordiv')
+//Loader 
+const btnSearch = () => {
     const inputField = document.getElementById('input-field')
     const inputFieldText = inputField.value
+    //Clear input
     inputField.value = ''
-    const url = `https://openlibrary.org/search.json?q=${inputFieldText}`
-    fetch(url)
-    .then(res => res.json())
-    .then(Data => displaySearch(Data.docs))
+
+    if (!inputFieldText) {
+        const errorText = document.getElementById('empty')
+        errorText.style.display = 'block'
+    }
+    else {
+        const emptyText = document.getElementById('empty')
+        emptyText.style.display = 'none'
+        const url = `https://openlibrary.org/search.json?q=${inputFieldText}`
+        fetch(url)
+            .then(res => res.json())
+            .then(Data => displaySearch(Data.docs))
+    }
 }
 
     // Show display search result
-const displaySearch = result =>{
+    const displaySearch = result => {
     const containerid = document.getElementById('container-id')
-    containerid.innerText = ''
+    
+    // Error Handling
+    if(!result.length){
+        errordiv.style.display = 'block'  
+    }
+    else{
+        errordiv.style.display = 'none'
+    }
+
+    //Total output Found
+    const totalOutput = document.getElementById('total')
+    totalOutput.innerHTML = `<p class ="text-center text-primary">Search Result Found: ${result.length}</p>`
+
+    //Clear previous serach result
+    containerid.textContent = ''
+
+    // loop 
     result.forEach(results => {
-        // console.log(results.author_name[0])
+        
         const div = document.createElement('div')
         div.classList.add('col')
-        div.innerHTML =`<div class="card mb-3 h-100" style="max-width: 540px;">
+        div.innerHTML = `<div class="card mb-3 h-100 p-2 carditem" style="max-width: 540px;">
         <div class="row g-0">
           <div class="col-md-4">
-            <img src="https://covers.openlibrary.org/b/id/${results.cover_i}-M.jpg" class="img-fluid rounded-start" alt="...">
+            <img src="https://covers.openlibrary.org/b/id/${results.cover_i}-M.jpg" class="img-fluid rounded-start" alt="img">
           </div>
           <div class="col-md-8">
             <div class="card-body">
-              <h5 class="card-title">Title: ${results.title}</h5>
-              <p class="card-text">Author: ${results.author_name}</p>
-              <p class="card-text"><small class="text-muted">Published Year: ${results.first_publish_year}</small></p>
+              <h5 class="card-title">Title: <span class="fw-bold">${results.title}</span></h5>
+              <p class="card-text">Author: <span class="fw-bold">${results.author_name}</span></p>
+              <p class="card-text"><small>Publisher: ${results.publisher}</small></p>
+              <p class="card-text"><small>First Published Year: ${results.first_publish_year}</small></p>
+              
             </div>
            </div>
         </div>
       </div>`
-      containerid.appendChild(div)
+        containerid.appendChild(div)
     });
 }
 
